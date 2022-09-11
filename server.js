2bd8b43139aae37e46a374cce0bb5748d2475eb6
Arman-Kabir <armankabir8@gmail.com>
2022-09-11 08:16:11 +0600

    basic set up done

---
+++ server.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+
+
+const app = require('./app');
+
+// database connection
+mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+    console.log(`Database connection is successful`  .red.bold);
+})
+
+// server
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`.yellow.bold);
+})
