2b6576558bde61a84a9ea97b194bb45fbb6e7f1e
Arman-Kabir <armankabir8@gmail.com>
2022-10-13 08:43:25 +0600

    User routes and others done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,12 +4,17 @@ exports.signup = async (req, res) => {
     try {
         const user = await signupService(req.body);
 
+        
+
         res.status(200).json({
             status:"success",
             message:"successfully signed up",
             data:user
         });
     } catch (error) {
-        
+        res.status(500).json({
+            status:"fail",
+            error,
+        })
     }
 }
