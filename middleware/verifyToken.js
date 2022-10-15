3b5859e8c1b58a54129ef4186187ef27a5e3e741
Arman-Kabir <armankabir8@gmail.com>
2022-10-15 19:31:16 +0600

    user persistence doing

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,4 +1,27 @@
+const jwt = require("jsonwebtoken");
+/**
+ * 1. check if token exists
+ * 2. if not token send res
+ * 3. decode the token
+ * 4. if valid next
+ * 
+ * 
+ */
 
 module.exports = async (req, res, next) => {
+    try {
+        const token = req.headers?.authorization?.split(" ")?.[1];
 
+        if (!token) {
+            return res.status(401).json({
+                status: "fail",
+                error: "You are not logged in"
+            })
+        }
+
+        const decoded = await jwt.verify()
+
+    } catch (error) {
+
+    }
 }
